e0b1b3e7a2dd07c8f3d4888b819d0fcc8c7fada6
Rachel Yelloz <rsy262618@gmail.com>
2025-02-02 22:10:29 +0200

    my first commit

---
+++ src/service.js
@@ -0,0 +1,58 @@
+import axios from 'axios';
+
+const apiUrl = require('../package.json').apiSettings.baseUrl;
+const api = axios.create({
+  baseURL: apiUrl,
+});
+
+// הוספת interceptor לתפיסת שגיאות
+api.interceptors.response.use(
+  response => {
+    // אם התגובה היא תקינה, מחזירים אותה
+    return response;
+  },
+  error => {
+    // כאן תופסים את השגיאות
+    console.error("שגיאה בבקשה:", error.response ? error.response.data : error.message);
+    return Promise.reject(error); // מחזירים את השגיאה
+  }
+);
+export default {
+  getTasks: async () => {
+    try {
+      const result = await api.get('/items');    
+      return result.data;
+    } catch (error) {
+      throw error; // טיפול בשגיאה יכול להתבצע כאן אם צריך
+    }
+  },
+
+  addTask: async(name) => {
+    console.log('addTask', name);
+    try {
+      const result = await api.post('/items', { name });
+      return result.data;
+    } catch (error) {
+      throw error;
+    }
+  },
+
+  setCompleted: async(id, isComplete) => {
+    console.log('setCompleted', { id, isComplete });
+    try {
+      const result = await api.put(`/items/${id}`, { Name: null, IsComplete: isComplete });
+      return result.data;
+    } catch (error) {
+      throw error;
+    }
+  },
+
+  deleteTask: async(id) => {
+    console.log('deleteTask', id);
+    try {
+      await api.delete(`/items/${id}`);
+    } catch (error) {
+      throw error;
+    }
+  }
+};
