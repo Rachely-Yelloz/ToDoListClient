c573a9e3a24d90ebefe4e4099cd62eeb58e53888
Rachel Yelloz <rsy262618@gmail.com>
2025-02-04 19:45:55 +0200

    api url

--- src/service.js
+++ src/service.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const apiUrl = require('../package.json').apiSettings.baseUrl;
+const apiUrl = process.env.REACT_APP_API_URL;
 const api = axios.create({
   baseURL: apiUrl,
 });
